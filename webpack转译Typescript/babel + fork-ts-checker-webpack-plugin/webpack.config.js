836449ed92189ae0d1925b07669f125ce15aeca4
huangzihang <huangzihang@lizhi.fm>
2022-04-02 09:51:34 +0800

    feat: add file

--- webpack转译Typescript/babel + fork-ts-checker-webpack-plugin/webpack.config.js
+++ webpack转译Typescript/babel + fork-ts-checker-webpack-plugin/webpack.config.js
@@ -4,22 +4,22 @@ module.exports = {
 	module: {
 		rules: [
 			{
-		        test: /\.(js|mjs|jsx|ts|tsx)$/,
-		        use: [
-		          {
-		            loader: 'babel-loader',
-		          },
-		        ],
-		        exclude: /node_modules/,
-		     },
+				test: /\.(js|mjs|jsx|ts|tsx)$/,
+				use: [
+					{
+						loader: 'babel-loader',
+					},
+				],
+				exclude: /node_modules/,
+			},
 		]
 	},
 	plugins: [
-						new ForkTsCheckerWebpackPlugin({
-							eslint: {
-								enabled: true,
-								files: '**/src/**/*.{ts,tsx}'
-							}
-						})
-	 ]
+		new ForkTsCheckerWebpackPlugin({
+			eslint: {
+				enabled: true,
+				files: '**/src/**/*.{ts,tsx}'
+			}
+		})
+	]
 }
